749fcbbf226482a248e46d35a08d83b665c3456a
Juha Makkonen <juha.makkonen@trademarknow.com>
2018-04-27 08:35:25 +0300

    Add weight normalization and pocketing the best weights thus far.

--- src/main/scala/com/untrackedfiles/ml/perceptron/LinearPerceptronTrainer.scala
+++ src/main/scala/com/untrackedfiles/ml/perceptron/LinearPerceptronTrainer.scala
@@ -3,17 +3,47 @@ package com.untrackedfiles.ml.perceptron
 import scala.annotation.tailrec
 import com.typesafe.scalalogging.LazyLogging
 
-case class LabeledSample(x: Array[Double], label: Int)
+import scala.io.Source
 
-case class Weights(w: Array[Double], b: Double)
+case class LabeledSample(x: Array[Double], label: Int) {
+  val length: Double = Weights.length(x)
+}
 
-class LinearPerceptronTrainer(eta: Double, epochs: Int) extends LazyLogging {
+case class Weights(w: Array[Double], b: Double) {
+  val length: Double = Weights.length(this)
+}
+
+object Weights {
+
+  def dontNormalize(weights: Weights): Weights = weights
+
+  def normalize(weights: Weights): Weights = {
+    val newWeights: Array[Double] = weights.w.map(_ / weights.length)
+    val newBias: Double = weights.b / weights.length
+
+    Weights(newWeights, newBias)
+  }
+
+  def length(weights: Weights): Double = {
+    math.sqrt((weights.w :+ weights.b).foldLeft(0.0d)((z, i) => z + i * i))
+  }
+
+  def length(x: Array[Double]): Double = {
+    math.sqrt(x.foldLeft(0.0d)((z, i) => z + i * i))
+  }
+
+}
+
+class LinearPerceptronTrainer(eta: Double,
+                              epochs: Int,
+                              normalization: (Weights => Weights) = Weights.normalize) extends LazyLogging {
 
   def train(data: List[LabeledSample]): (Weights, Int) = {
     val stamp: Long = System.currentTimeMillis()
     val dim = data.head.x.size
     val radius = resolveRadius(data)
-    val (weights, errors) = iterate(epochs, data, Weights(Array.ofDim[Double](dim), 0.0d), radius)
+    val (weights, errors) = iterate(epochs, data, Weights(Array.ofDim[Double](dim), 0.0d), radius,
+      (Weights(Array.ofDim[Double](dim), 0.0d), data.size), normalization)
     val elapsed: Long = System.currentTimeMillis() - stamp
 
     logger.info(s"Finished in ${elapsed} ms (weights: [${weights.w.mkString(",")}, ${weights.b}], errors: $errors)")
@@ -21,25 +51,44 @@ class LinearPerceptronTrainer(eta: Double, epochs: Int) extends LazyLogging {
   }
 
   @tailrec
-  final def iterate(epoch: Int, data: List[LabeledSample], weights: Weights, r: Double): (Weights, Int) = {
-    val (adjWeights, errors) = trial(data, weights, r)
-    logger.info(s"epoch ${epochs - epoch + 1} : (weights: [${weights.w.mkString(",")}, ${weights.b}], errors: $errors)")
-    if (epoch == 0 || errors == 0) {
+  final def iterate(epoch: Int,
+                    data: List[LabeledSample],
+                    weights: Weights,
+                    radius: Double,
+                    pocket: (Weights, Int),
+                    normalization: (Weights => Weights)): (Weights, Int) = {
+    val (adjWeights, errors) = {
+      val trialResults = trial(data, weights, radius)
+
+      (normalization(trialResults._1), trialResults._2)
+    }
+    val newPocket = if (errors < pocket._2) {
       (adjWeights, errors)
     } else {
-      iterate(epoch - 1, data, adjWeights, r)
+      pocket
+    }
+
+    logger.info(s"epoch ${epoch} : (weights: [${weights.w.mkString(",")}, ${weights.b}], errors: $errors)")
+    if (errors == 0) {
+      (adjWeights, errors)
+    } else if (epoch == 0) {
+      pocket
+    } else {
+      iterate(epoch - 1, data, adjWeights, radius, newPocket, normalization)
     }
   }
 
-  def trial(data: List[LabeledSample], weights: Weights, r: Double): (Weights, Int) = {
+  def trial(data: List[LabeledSample], weights: Weights, radius: Double): (Weights, Int) = {
     data.foldLeft(weights, 0) { (acc, s) =>
       val p = innerProduct(s.x, weights.w) + weights.b
-      logger.trace(s"trial : (sample: ${s.x.mkString(",")}, " +
-        s"weights: [${weights.w.mkString(",")}, ${weights.b}] ) => $p")
+      logger.trace(s"trial : (sample: ${s.x.mkString(",")}, weights: [${weights.w.mkString(",")}, ${weights.b}] ) => $p")
 
       if (p * s.label <= 0) {
-        (Weights(acc._1.w.zip(s.x).map(w => w._1 + eta * s.label * w._2),
-          acc._1.b + (eta * s.label * r * r)), acc._2 + 1)
+        (Weights(
+          acc._1.w.zip(s.x).map(w => w._1 + (eta * s.label * w._2)),
+          acc._1.b + (eta * s.label)
+        ),
+          acc._2 + 1)
       } else {
         acc
       }
@@ -54,4 +103,28 @@ class LinearPerceptronTrainer(eta: Double, epochs: Int) extends LazyLogging {
   def resolveRadius(data: List[LabeledSample]): Double = {
     data.map(x => math.sqrt(innerProduct(x.x, x.x))).max
   }
+
+}
+
+object LinearPerceptronTrainer {
+
+  def readLabeled2DData(src: Source, separator: String = "\t"): List[LabeledSample] = {
+    src.getLines().map(_.split(separator)).map {
+      case Array(x1, x2, y) => LabeledSample(Array[Double](x1.toDouble, x2.toDouble), y.toInt)
+    }.toList
+  }
+
+  def translateLabels(samples: List[LabeledSample]): List[LabeledSample] = {
+    samples.map { sample =>
+      val label = if (sample.label > 0) {
+        1
+      } else {
+        -1
+      }
+
+      LabeledSample(sample.x, label)
+    }
+  }
+
+
 }
